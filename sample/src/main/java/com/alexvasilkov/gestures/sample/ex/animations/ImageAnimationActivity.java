1055421aae2e3cc3745c18361b7716de6046eb37
lizheng9 <lizheng9@vipkid.com.cn>
2023-12-05 18:21:01 +0800

    feat: 多view放大

--- sample/src/main/java/com/alexvasilkov/gestures/sample/ex/animations/ImageAnimationActivity.java
+++ sample/src/main/java/com/alexvasilkov/gestures/sample/ex/animations/ImageAnimationActivity.java
@@ -20,10 +20,10 @@ public class ImageAnimationActivity extends BaseSettingsActivity {
 
     private static final int PAINTING_ID = 2;
 
-    private ImageView image;
+    private ImageView image, image2;
     private GestureImageView fullImage;
     private View fullBackground;
-    private ViewsTransitionAnimator<?> animator;
+    private ViewsTransitionAnimator<?> animator, animator2, cur;
 
     private Painting painting;
 
@@ -34,6 +34,7 @@ public class ImageAnimationActivity extends BaseSettingsActivity {
         initContentView();
 
         image = findViewById(R.id.single_image);
+        image2 = findViewById(R.id.single_image2);
         fullImage = findViewById(R.id.single_image_full);
         fullBackground = findViewById(R.id.single_image_back);
 
@@ -43,13 +44,20 @@ public class ImageAnimationActivity extends BaseSettingsActivity {
         // Loading image
         painting = Painting.list(getResources())[PAINTING_ID];
         GlideHelper.loadThumb(image, painting.thumbId);
+        GlideHelper.loadThumb(image2, painting.thumbId);
 
         // We will expand image on click
-        image.setOnClickListener(view -> openFullImage());
+        image.setOnClickListener(view -> {
+            openFullImage(animator);
+        });
+        image2.setOnClickListener(view -> openFullImage(animator2));
 
         // Initializing image animator
         animator = GestureTransitions.from(image).into(fullImage);
         animator.addPositionUpdateListener(this::applyImageAnimationState);
+
+        animator2 = GestureTransitions.from(image2).into(fullImage);
+        animator2.addPositionUpdateListener(this::applyImageAnimationState);
     }
 
     /**
@@ -63,8 +71,8 @@ public class ImageAnimationActivity extends BaseSettingsActivity {
     @Override
     public void onBackPressed() {
         // We should leave full image mode instead of closing the screen
-        if (!animator.isLeaving()) {
-            animator.exit(true);
+        if (!cur.isLeaving()) {
+            cur.exit(true);
         } else {
             super.onBackPressed();
         }
@@ -78,7 +86,7 @@ public class ImageAnimationActivity extends BaseSettingsActivity {
         fullImage.getController().resetState();
     }
 
-    private void openFullImage() {
+    private void openFullImage(ViewsTransitionAnimator<?> animator2) {
         // Setting image drawable from 'from' view to 'to' to prevent flickering
         if (fullImage.getDrawable() == null) {
             fullImage.setImageDrawable(image.getDrawable());
@@ -89,7 +97,8 @@ public class ImageAnimationActivity extends BaseSettingsActivity {
         // Resetting to initial image state
         fullImage.getController().resetState();
 
-        animator.enterSingle(true);
+        animator2.enterSingle(true);
+        cur = animator2;
         GlideHelper.loadFull(fullImage, painting.imageId, painting.thumbId);
     }
 
